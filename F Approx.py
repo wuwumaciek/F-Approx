7cfa0a19ead31f0f0623169123c23c1e20b90c27
wuwumaciek <66557585+wuwumaciek@users.noreply.github.com>
2020-09-05 23:57:01 +0200

    Fixing logharitmic and exponential functions

--- F Approx.py
+++ F Approx.py
@@ -1,6 +1,6 @@
 import sympy
 from sympy.utilities.lambdify import lambdify
-from fractions import Fraction
+from math import factorial as silnia
 
 def Convert(s):
     new = ""
@@ -16,11 +16,6 @@ def derivative(function, n):
         f=f_temp
     return(f)
 
-def silnia(n):
-    if n>1:
-        return n*silnia(n-1) 
-    elif n in (0,1):
-        return 1
 
 #   print(derivative(input(), int(input())))
 
@@ -37,75 +32,30 @@ for n in range (a):
     dx = (derivative(f, n))
     ddx = lambdify(x, dx)
     list1.append(ddx(c))
-if(c==0):
-    f=0
-    for n in range (a):
-        polynomial0.append(Fraction(int(list1[n]), int(silnia(n))))
-        if int(list1[n]) != 0:
-            if f==0 and n>0:
-                if n>1:
-                    temp = "(" + str(polynomial0[n]) + ")" + "x^" + str(n)
-                    polynomial.append(temp)
-                elif n == 1:
-                    temp = "(" + str(polynomial0[n]) + ")" + "x"
-                    polynomial.append(temp)
-                elif n == 0:
-                    temp = "(" + str(polynomial0[n]) + ")"
-                    polynomial.append(temp)
-            else:
-                if n>1:
-                    temp = " + (" + str(polynomial0[n]) + ")" + "x^" + str(n)
-                    polynomial.append(temp)
-                elif n == 1:
+
+print(list1)
+f=0
+for n in range (a):
+    polynomial0.append((float(list1[n]))/(float(silnia(n))))
+    if float(list1[n]) != 0:
+        if f==0 and n>0:
+            if n>1:
+                temp = "(" + str(polynomial0[n]) + ")" + "x^" + str(n)
+                polynomial.append(temp)
+            elif n == 1:
+                temp = "(" + str(polynomial0[n]) + ")" + "x"
+                polynomial.append(temp)
+        else:
+            if n>1:
+                temp = " + (" + str(polynomial0[n]) + ")" + "x^" + str(n)
+                polynomial.append(temp)
+            elif n == 1:
                     temp = " + (" + str(polynomial0[n]) + ")" + "x"
                     polynomial.append(temp)
-                elif n == 0:
-                    temp = "(" + str(polynomial0[n]) + ")"
-                    polynomial.append(temp)
-            f+=1
-
-    out = Convert(polynomial)
-    print(out)
-else:
-    f=0
-    for n in range (a):
-        #   print("%")
-        #   print(list1[n])
-        #   print(type(list1[n]))
-        #   print("%")
-        #   print(silnia(n))
-        #   print(type(silnia(n)))
-        #   print("%")
-        #   print((list1[n])/(silnia(n)))
-        polynomial0.append((list1[n])/(silnia(n)))
-        
-        if float(list1[n]) != 0:
-            if f==0 and n>0:
-                if n>1:
-                    temp = "(" + str(polynomial0[n]) + ")" + "(x-" + str(c) + ")^" + str(n)
-                    polynomial.append(temp)
-                elif n == 1:
-                    temp = "(" + str(polynomial0[n]) + ")" + "(x-" + str(c) + ")"
-                    polynomial.append(temp)
-                elif n == 0:
-                    temp = "(" + str(polynomial0[n]) + ")"
-                    polynomial.append(temp)
-            else:
-                if n>1:
-                    temp = " + (" + str(polynomial0[n]) + ")" + "(x-" + str(c) + ")^" + str(n)
-                    polynomial.append(temp)
-                elif n == 1:
-                    temp = " + (" + str(polynomial0[n]) + ")" + "(x-" + str(c) + ")"
-                    polynomial.append(temp)
-                elif n == 0:
-                    temp = "(" + str(polynomial0[n]) + ")"
-                    polynomial.append(temp)
-            f+=1
-
-    #   print(polynomial0)
-    #   print(polynomial)
-    #   print(f)
-    out = Convert(polynomial)
-
-    print(out)
-
+            elif n == 0:
+                temp = "(" + str(polynomial0[n]) + ")"
+                polynomial.append(temp)
+        f+=1
+print(polynomial0)
+out = Convert(polynomial)
+print(out)
